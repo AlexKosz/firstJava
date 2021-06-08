c74e17e943bd7beb918a4250743e6998ea4113cc
Zanzong43 <alexkoszuta43@gmail.com>
2021-06-08 15:15:08 -0400

    Initial Commit

---
+++ FirstJava.java
@@ -0,0 +1,7 @@
+public class YourJavaProgram {
+    public static void main(String[] args) {
+        System.out.println("My name is Alexander");
+        System.out.println("I am 19 years old");
+        System.out.println("My hometown is Buffalo");
+    }
+}
